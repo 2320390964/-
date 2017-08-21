cf9b80790ac5362bb3eb4103f7436feb8ee62ff1
阿星 <2320390964@qq.com>
2017-08-21 12:52:58 +0800

    提交

--- player/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header("Content-Type: text/html; charset=utf-8");
-	session_start();
-	include("conn.php");
-	$res=$_GET['arr'] ;
-	$sql="delete from `list` where id='$res'";
-	mysql_query($sql);
-	mysql_close($conn);
-?>
--- player/search.php
+++
@@ -1,7 +0,0 @@
-﻿
-<?php
-//搜索
-$url=$_POST['urlString'];
-$res = file_get_contents($url);
-echo $res;
-?>
