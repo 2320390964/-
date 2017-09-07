661eb2534b513073107d3cc7129b32a3c6611a1d
阿星 <2320390964@qq.com>
2017-09-07 13:49:40 +0800

    huise

---
+++ 灰调网页UI/js/index.js
@@ -0,0 +1,35 @@
+var num;
+var index;
+window.onload=init;
+function init()
+{
+	num=5;
+	index=0;
+}
+function nextPic()
+{
+	//do something to change the pic
+	index++;
+	changeFlower();
+}
+function changeFlower()
+{
+	index=index%5;
+	 var div = document.getElementById("info");
+    div.style.background="url(images/"+index+".png) no-repeat right";
+	var html='<img src="images/'+index+'.png" width="618px" height="301px">';
+	$('#flower').html(String(html));
+	//小圆点改变第index
+	var list = $('#point li');
+	/*console.log(list)*/
+    list.removeClass('selected');
+    list[index].className+='selected';
+}
+function prePic()
+{
+	index--;
+	if(index==-1)
+		index=4;
+	changeFlower();
+
+}
